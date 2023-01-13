921214c7063904c1e91b4ded065c1d55b8ddf543
ROGERIO RUFINO <92214783+RogerioRuffino@users.noreply.github.com>
2023-01-13 16:07:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,45 +18,29 @@ function myResult(){
 
   
     if(option_from === "kg" && option_to==="ton"){
-		//this is kg to ton formula 
 		result.value = Number(input.value) * 0.001;
 	}else if(option_from === "kg" && option_to==="@"){
-
-		//this is kg to @ formula 
 		result.value = Number(input.value) / 30;
-
 	}else if(option_from === "kg" && option_to==="kg"){
-		//this is kg to kg formula 
 		result.value = input.value
 	}
 
 
 
 	if(option_from === "ton" && option_to==="kg"){
-		//this is ton to kg formula 
 		result.value = Number(input.value) * 1000;
-	}else if(option_from === "ton" && option_to==="@"){
-		//this is ton to @ formula 
-		result.value = Number(input.value) * 100000;
 	}else if(option_from === "ton" && option_to==="ton"){
-		//this is ton to ton formula 
-		result.value = input.value
+		result.value = Number(input.value);
+	}else if(option_from === "ton" && option_to==="@"){
+		result.value = (Number(input.value)*1000)/30;
 	}
 
-	if(option_from === "ton" && option_to==="@"){
-		//this is @ to ton formula 
-		result.value = Number(input.value) / 30;
+	if(option_from === "@" && option_to==="@"){
+	result.value = Number(input.value);
 	}else if(option_from === "@" && option_to==="kg"){
-		//this is @ to kg formula 
-		result.value = Number(input.value) * 30;
-	}else if(option_from === "@" && option_to==="@"){
-		//this is @ to @ formula 
-		result.value = input.value
+	result.value = Number(input.value) * 30;
+	}else if(option_from === "@" && option_to==="ton"){
+	result.value = (Number(input.value)*30)/1000;
 	}
-	
-
-
-
-
 
 }
